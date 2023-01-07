b9ea129fa4b9c211ec9dfa40977e803819528cc4
Sviatoslav Dumchenko <zcbr95+github@gmail.com>
2023-01-07 10:27:40 +0300

    feat: wrap responsewriter

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package traefik_plugin_add_response_header
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"io"
 	"net/http"
-	"net/http/httptest"
-	"os"
 )
 
 type Config struct {
@@ -24,21 +23,40 @@ type plugin struct {
 	config *Config
 }
 
-func (p *plugin) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	resp := httptest.NewRecorder()
+type wrappedResponseWriter struct {
+	w    http.ResponseWriter
+	buf  *bytes.Buffer
+	code int
+}
 
-	os.Stdout.WriteString(fmt.Sprintf("ServeHTTP: w headers before - %+v", w.Header()))
+func (w wrappedResponseWriter) Header() http.Header {
+	return w.w.Header()
+}
+
+func (w wrappedResponseWriter) Write(b []byte) (int, error) {
+	return w.buf.Write(b)
+}
+
+func (w wrappedResponseWriter) WriteHeader(code int) {
+	w.code = code
+}
+
+func (w wrappedResponseWriter) Flush() {
+	io.Copy(w.w, w.buf)
+	w.w.WriteHeader(w.code)
+}
+
+func (p *plugin) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	resp := wrappedResponseWriter{
+		w:    w,
+		buf:  &bytes.Buffer{},
+		code: 200,
+	}
 
 	p.next.ServeHTTP(resp, req)
 
-	for k := range resp.Header() {
-		w.Header().Set(k, resp.Header().Get(k))
-	}
-	w.Header().Set(p.config.To, req.Header.Get(p.config.From))
-
-	io.Copy(w, resp.Body)
-
-	w.WriteHeader(resp.Code)
+	resp.Header().Set(p.config.To, req.Header.Get(p.config.From))
+	resp.Flush()
 }
 
 func New(_ context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
