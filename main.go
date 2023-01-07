2cae6e9a6f33afd922a74da9dc00ba1e23bdc276
Sviatoslav Dumchenko <zcbr95+github@gmail.com>
2023-01-07 09:50:05 +0300

    fix: try to use trailer prefix

--- main.go
+++ main.go
@@ -23,14 +23,14 @@ type plugin struct {
 }
 
 func (p *plugin) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	w.Header().Add("Trailer", p.config.To)
-
-	os.Stdout.WriteString(fmt.Sprintf("ServeHTTP: request headers before - %+v", req.Header))
 	p.next.ServeHTTP(w, req)
-	os.Stdout.WriteString(fmt.Sprintf("ServeHTTP: request headers after - %+v", req.Header))
 
 	src := req.Header.Get(p.config.From)
-	w.Header().Set(p.config.To, src)
+	os.Stdout.WriteString(fmt.Sprintf("ServeHTTP: src header - %+v", src))
+
+	if src != "" {
+		w.Header().Set(http.TrailerPrefix+p.config.To, src)
+	}
 }
 
 func New(_ context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
